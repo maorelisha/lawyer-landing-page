19b38dce93076f2369e27bb65b2243275c239930
Maor Elisha <maorelisha464@gmail.com>
2018-12-18 13:07:29 +0200

    Updates

---
+++ precache-manifest.88c26128df2b86e04a1848676377b83d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "86af9868af61c1159b9a33c0a8ac7170",
+    "url": "/lawyer-landing-page/static/media/asaf.86af9868.png"
+  },
+  {
+    "revision": "fa784d8ce6eaf87f4caf",
+    "url": "/lawyer-landing-page/static/css/main.a6aca120.chunk.css"
+  },
+  {
+    "revision": "fcd0f058c6a56f65ea0a87c8e5f481a0",
+    "url": "/lawyer-landing-page/static/media/qaBack.fcd0f058.png"
+  },
+  {
+    "revision": "4a5f92aa5bb29a608acb",
+    "url": "/lawyer-landing-page/static/js/1.4a5f92aa.chunk.js"
+  },
+  {
+    "revision": "9e79a7ec36b001821660",
+    "url": "/lawyer-landing-page/static/js/runtime~main.9e79a7ec.js"
+  },
+  {
+    "revision": "11d043aeeb08d32ce305de7ff650a49b",
+    "url": "/lawyer-landing-page/static/media/customer.11d043ae.png"
+  },
+  {
+    "revision": "fa784d8ce6eaf87f4caf",
+    "url": "/lawyer-landing-page/static/js/main.fa784d8c.chunk.js"
+  },
+  {
+    "revision": "fc3984c747842c196d97a8a0ab624bd7",
+    "url": "/lawyer-landing-page/static/media/headerback.fc3984c7.png"
+  },
+  {
+    "revision": "1ef218e589349befdbeb4dff66bb99c6",
+    "url": "/lawyer-landing-page/static/media/between.1ef218e5.png"
+  },
+  {
+    "revision": "82af3950ff395547adf0d7fd77d6d94a",
+    "url": "/lawyer-landing-page/static/media/footer.82af3950.png"
+  },
+  {
+    "revision": "4a5f92aa5bb29a608acb",
+    "url": "/lawyer-landing-page/static/css/1.9532e7dd.chunk.css"
+  },
+  {
+    "revision": "03a0fe7962933705e60d2ecb2d377169",
+    "url": "/lawyer-landing-page/index.html"
+  }
+];
